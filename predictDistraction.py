0428b9edde3b5f5a9469d1765096e58b270f20b7
rishi <rishikumarpebbeti@gmail.com>
2023-11-30 14:03:50 -0700

    Added comments

--- predictDistraction.py
+++ predictDistraction.py
@@ -1,4 +1,4 @@
 
-
+# Stubbed methods, I need to integrate with the ML models of other team members which belongs to Project 5
 def getDistraction(image):
     return "distracted"
--- predictEmotion.py
+++ predictEmotion.py
@@ -1,3 +1,4 @@
 
+# Stubbed methods, I need to integrate with the ML models of other team members which belongs to Project 5
 def getEmotion(image):
     return "drowsy"
